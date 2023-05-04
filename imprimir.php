abf1fec684ce691c6bad70f95df5c439a38056b3
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-05-04 20:49:58 -0300

    up

--- imprimir.php
+++ imprimir.php
@@ -1,18 +1,18 @@
 <h2>Materias:</h2>
 <?php 
-    foreach($_GET['materia'] as $materia){
+    foreach($_POST['materia'] as $materia){
         echo $materia . "<br>";
     }
 ?>
 <h2>Linhas:</h2>
 <?php 
-    foreach($_GET['linha'] as $linha){
+    foreach($_POST['linha'] as $linha){
         echo $linha . "<br>";
     }
 ?>
 <h2>Coluna:</h2>
 <?php 
-    foreach($_GET['coluna'] as $coluna){
+    foreach($_POST['coluna'] as $coluna){
         echo $coluna . "<br>";
     }
 ?>
